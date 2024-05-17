115a04c5607cdd8df2518e0d3d61b8c645997ab6
KimTeaSick <notseph8556@gmail.com>
2024-05-17 22:33:57 +0900

    initial commit

---
+++ src/components/headless/Input.tsx
@@ -0,0 +1,41 @@
+import { ChangeEvent, createContext, useContext } from "react"
+import { TextInput } from "react-native"
+
+type InputContextType = {
+  placeholder: string
+  inputValue: string
+  initialValue: string
+  type: 'text' | 'number' | 'password'
+  onChange: (text: string) => void
+}
+
+type InputType = InputContextType & React.PropsWithChildren<{}>
+
+const InputContext = createContext({ placeholder: '', initialValue: '', inputValue: '', type: 'text', onChange: (text: string) => { }, })
+
+const useInputContext = () => {
+  const context = useContext(InputContext)
+  return context
+}
+
+const Input = ({ placeholder, initialValue, inputValue, type, onChange, children }: InputType) => {
+  const value = { placeholder, initialValue, inputValue, type, onChange }
+  return (
+    < InputContext.Provider value={value}>
+      {children}
+    </InputContext.Provider>
+  )
+}
+
+const InputBody = () => {
+  const { inputValue, placeholder, onChange } = useInputContext()
+  return (
+    <TextInput
+      value={inputValue}
+      placeholder={placeholder}
+      onChangeText={(text: string) => { onChange(text) }}
+    />
+  )
+}
+
+let type: string | number = ''
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import { View } from "react-native";
+import styled from "styled-components/native";
+
+const Texxt = styled.Text`
+padding: 4px;
+color: #fff;
+`
+
+const Main = () => (
+  <View>
+    <Texxt>asd</Texxt>
+  </View>
+)
+
+export default Main
